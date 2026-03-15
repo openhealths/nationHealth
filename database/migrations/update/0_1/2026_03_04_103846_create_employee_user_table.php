lemadior/ohealth

i#42: add User & Employee ManyToMany relation

database/migrations/update/0_1/2026_03_04_103846_create_employee_user_table.php
@@ -1,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('employee_user')) {
+            Schema::create('employee_users', function (Blueprint $table) {
+                $table->foreignId('employee_id')->constrained()->onDelete('cascade');
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+                // Set the primary key to be a combination of employee_id and user_id
+                $table->primary(['employee_id', 'user_id']);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasTable('employee_users')) {
+            Schema::dropIfExists('employee_users');
+        }
+    }
+};

openhealths/nationHealth

database/migrations/update/0_1/2026_03_04_103846_create_employee_user_table.php
@@ -1,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('employee_users')) {
+            Schema::create('employee_users', function (Blueprint $table) {
+                $table->foreignId('employee_id')->constrained()->onDelete('cascade');
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+                // Set the primary key to be a combination of employee_id and user_id
+                $table->primary(['employee_id', 'user_id']);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasTable('employee_users')) {
+            Schema::dropIfExists('employee_users');
+        }
+    }
+};